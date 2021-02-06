ea070fd8ecc5a6381a546ed22aaccad2f8118f9e
gunaalan <elangogunaalan@gmail.com>
2021-02-06 12:28:32 +0530

    Adding func to store and retrieve btn

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("Browser Storage");
-console.log("Hi");
+const storeBtn = document.getElementById("store-btn");
+const retrieveBtn = document.getElementById("retrieve-btn");
+
+storeBtn.addEventListener("click", () => {
+  localStorage.setItem("userId", "1");
+});
+
+retrieveBtn.addEventListener("click", () => {
+  const userId = localStorage.getItem("userId");
+  console.log(userId);
+});
